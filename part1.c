55db1f1dfc4401223659f6f9f5d9afbb50bed0be
roman2veces <romanroman2705@gmail.com>
2023-09-26 19:52:09 -0400

    initial commit

---
+++ part1.c
@@ -0,0 +1,21 @@
+/*
+ * processlab - part1.c
+ *
+ * Ecole polytechnique de Montreal, GIGL, Automne  2022
+ * vos noms, prénoms et matricules
+*/
+
+#include "libprocesslab/libprocesslab.h"
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+void question1()
+{
+   
+    // TODO
+}
+
