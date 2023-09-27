5789e7486c453bed67cd47a201837fdaaa298462
Roman Roman <romanroman2705@gmail.com>
2023-09-26 21:04:49 -0400

    create process three in question 1.1

--- part1.c
+++ part1.c
@@ -15,7 +15,84 @@
 
 void question1()
 {
-   
-    // TODO
+    pid_t process_1_1;
+    pid_t process_1_2;
+    pid_t process_1_3;
+
+    if ((process_1_1 = fork()) == 0) {
+        printf("LEVEL 1.1 PROCESS: started. Waiting for 2.1 \n");
+        
+        pid_t process_2_1;
+        if ((process_2_1 = fork()) == 0) {
+            printf("LEVEL 2.1 PROCESS: started. \n");
+            printf("LEVEL 2.1 PROCESS: finished. \n");
+            _exit(0);
+        }
+
+        waitpid(process_2_1, NULL, 0);
+        printf("LEVEL 1.1 PROCESS: finished waiting. \n");
+        _exit(0);
+    }
+
+    if ((process_1_2 = fork()) == 0) {
+        printf("LEVEL 1.2 PROCESS: started. Waiting for 2.2 and 2.3 \n");
+        
+        id_t process_2_2;
+        id_t process_2_3;
+        if ((process_2_2 = fork()) == 0) {
+            printf("LEVEL 2.2 PROCESS: started. \n");
+            printf("LEVEL 2.2 PROCESS: finished. \n");
+            _exit(0);
+        }
+
+        if ((process_2_3 = fork()) == 0) {
+            printf("LEVEL 2.3 PROCESS: started. \n");
+            printf("LEVEL 2.3 PROCESS: finished. \n");
+            _exit(0);
+        }
+        
+        waitpid(process_2_2, NULL, 0);
+        waitpid(process_2_3, NULL, 0);
+        printf("LEVEL 1.2 PROCESS: finished waiting. \n");
+        _exit(0);
+    }
+
+    if ((process_1_3 = fork()) == 0) {
+        printf("LEVEL 1.3 PROCESS: started. Waiting for 2.4, 2.5 and 2.6 \n");
+
+        id_t process_2_4;
+        id_t process_2_5;
+        id_t process_2_6;
+        if ((process_2_4 = fork()) == 0) {
+            printf("LEVEL 2.4 PROCESS: started. \n");
+            printf("LEVEL 2.4 PROCESS: finished. \n");
+            _exit(0);
+        }
+
+        if ((process_2_5 = fork()) == 0) {
+            printf("LEVEL 2.5 PROCESS: started. \n");
+            printf("LEVEL 2.5 PROCESS: finished. \n");
+            _exit(0);
+        }
+
+        if ((process_2_6 = fork()) == 0) {
+            printf("LEVEL 2.6 PROCESS: started. \n");
+            printf("LEVEL 2.6 PROCESS: finished. \n");
+            _exit(0);
+        }
+        
+        waitpid(process_2_4, NULL, 0);
+        waitpid(process_2_5, NULL, 0);
+        waitpid(process_2_6, NULL, 0);
+        printf("LEVEL 1.3 PROCESS: finished waiting. \n");
+        _exit(0);
+    } 
+    
+    printf("LEVEL 0 PROCESS: waiting for children process. \n");
+    // TODO: There is some macros to use instead of 0 here, which should I use? 
+    waitpid(process_1_1, NULL, 0);
+    waitpid(process_1_2, NULL, 0);
+    waitpid(process_1_3, NULL, 0);
+    printf("LEVEL 0 PROCESS: finished waiting. \n");       
 }
 
