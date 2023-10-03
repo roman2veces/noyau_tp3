1ea04371edb73ce185a8359ff39cf14f35a5faa9
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2023-10-03 13:35:22 -0400

    add part2

--- part2.c
+++ part2.c
@@ -3,7 +3,7 @@
  *
  * Ecole polytechnique de Montreal, GIGL, Automne  2022
  * vos noms, prénoms et matricules
-*/
+ */
 
 // TODO
 // Si besoin, ajouter ici les directives d'inclusion
@@ -19,22 +19,42 @@
 // nb est le nombre de threads qui vont contribuer au calcul
 #define nb 4
 
-//tableau somme est utilisé pour le calcul des sommes patielles par les threads
+// tableau somme est utilisé pour le calcul des sommes patielles par les threads
 long somme[nb];
 
-
 // fonction exécutée par chaque thread créé
-void* contribution(void*p)
+void *contribution(void *p)
 {
-    // TODO
-
+  // TODO
+  int no = *(int *)p;
+  somme[no] = 0;
+  long start = 1 + ((no * (long)m) / nb);
+  long end = ((no + 1) * (long)m) / nb;
+  for (long i = start; i <= end; i++)
+    somme[no] += i;
   return NULL;
 }
 
-
-void question2( )
+void question2()
 {
-    // TODO
-    
-}
+  // TODO
+  pthread_t threadArray[nb];
+  int thread_args[nb];
+  for (size_t no = 0; no < nb; no++)
+  {
+    thread_args[no] = no;
+    pthread_create(&threadArray[no], NULL, contribution, (void *)&thread_args[no]);
+  }
 
+  long totalSum = 0;
+  long TotalSumWithFormula = ((long)m * (long)(m + 1)) / 2;
+  for (size_t no = 0; no < nb; no++)
+  {
+    pthread_join(threadArray[no], NULL);
+    totalSum += somme[no];
+  }
+  int no = nb;
+
+  printf("\nLa somme avec les thread est:\t%lu\n", totalSum);
+  printf("\nLa somme avec la formule est:\t%lu\n", TotalSumWithFormula);
+}
